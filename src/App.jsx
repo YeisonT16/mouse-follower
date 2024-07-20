f721b7a1220ad0eb40ddbaa0a8b2dfd9e93b5ef3
Yeison Silva <yeisonsilvat06@gmail.com>
2024-07-19 22:30:36 -0500

    Estilos y estado añadidos

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState(false)
+
+  
+    useEffect(() => {
+      console.log('effect ',{ enabled })
+      const handleMove = (event) => {
+        const { clientX, clientY } = event
+        console.log('handleMove ', { clientX, clientY });      
+      }
+      if(enabled) {
+        window.addEventListener('pointermove', handleMove)
+      }
+    }, [enabled] )
+  
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <main>
+    <div className='absolute bg-blue-700 rounded-full opacity-80 pointer-events-none -left-5 -top-5 size-10'></div>
+    <button onClick={() => {setEnabled(!enabled)}}>
+      {enabled ? 'Desactivar' : 'Activar'} Seguir puntero
+    </button>
+  </main>
   )
 }
 
