a54417988d0f7db941f3a72c446a07201d5ceed9
Yeison Silva <yeisonsilvat06@gmail.com>
2024-07-20 23:47:19 -0500

    Metodo para remover los eventos y/o suscripciones.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,29 @@ import './App.css'
 
 function App() {
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
 
   
     useEffect(() => {
       console.log('effect ',{ enabled })
       const handleMove = (event) => {
         const { clientX, clientY } = event
-        console.log('handleMove ', { clientX, clientY });      
+        console.log('handleMove ', { clientX, clientY });
+        setPosition({x:clientX, y: clientY})     
       }
+
       if(enabled) {
         window.addEventListener('pointermove', handleMove)
       }
+      //cleanup:
+      //-> cuando el componente se desmontó
+      //-> cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+      return () => {
+          console.log('cleanup')
+          window.removeEventListener('pointermove', handleMove)
+      }
+
+
     }, [enabled] )
   
 
@@ -22,7 +34,7 @@ function App() {
 
   return (
   <main>
-    <div className='absolute bg-blue-700 rounded-full opacity-80 pointer-events-none -left-5 -top-5 size-10'></div>
+    <div className={`absolute bg-blue-700 rounded-full opacity-80 pointer-events-none -left-5 -top-5 size-10 translate-x-[${position.x}px] translate-y-[${position.y}px]`}></div>
     <button onClick={() => {setEnabled(!enabled)}}>
       {enabled ? 'Desactivar' : 'Activar'} Seguir puntero
     </button>
